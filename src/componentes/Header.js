fa765f047b9dc3af2470759c8e8a8b0e8957f001
Jorge Garcia <jorgejgga09@gmail.com>
2023-12-18 12:21:55 -0400

    resolviendo problema de estilos en el header

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -2,6 +2,15 @@ import { useNavigate } from "react-router-dom";
 import Formulario from "./Form";
 import styled from "styled-components";
 
+const Cabecera = styled.header`
+  background-color: #313131; /* o el color que desees */
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  padding: 30px 0px
+`;
+
 
 
 const Header = ({ setBusqueda, setBusquedaTipo, reset }) => {
@@ -10,24 +19,21 @@ const Header = ({ setBusqueda, setBusquedaTipo, reset }) => {
 
     const tiposPokemon = ["Normal", "Fire", "Water", "Electric", "Grass", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"];
 
-    const Cabecera = styled.header`
-      background: #313131
-    `
-
     return(
-        <Header>
+        <Cabecera>
 
         <h2>Buscar</h2>
         <Formulario setBusqueda={setBusqueda} />
 
         <button onClick={() => reset()}>Resetear</button>
+        <div>
+          {tiposPokemon.map(tipo => (
+            <button key={tipo} onClick={() => setBusquedaTipo(tipo)}>{tipo}</button>
+          ))}
 
-        {tiposPokemon.map(tipo => (
-          <button key={tipo} onClick={() => setBusquedaTipo(tipo)}>{tipo}</button>
-        ))}
-
-        <button onClick={() => navigate(-1)}>back</button>
-      </Header>
+          <button onClick={() => navigate(-1)}>back</button>
+      </div>
+      </Cabecera>
     )
 
 }
